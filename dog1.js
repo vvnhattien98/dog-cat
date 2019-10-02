669c4a7031b2cb980ecb6091fa615c180fffcbd3
tien <vvnhattien98@gmail.com>
2019-10-02 16:34:44 +0700

    let console log

--- dog1.js
+++ dog1.js
@@ -1,5 +1,6 @@
-function Dog1(){
+function Dog1(name){
 	this.stomach = [];
+	this.name = name;
 }
 
 Dog1.prototype.eat = function(eat) {
---
+++ index.js
@@ -0,0 +1,7 @@
+// var dog1 = require('./dog1');
+// var cat = require('./cat');
+
+// var dog1 = new dog1('Tom');
+// var tom = new cat();
+// dog1.eat(tom);
+console.log("Tien");
